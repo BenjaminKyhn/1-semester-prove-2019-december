b7d0fb147b03a01a288a7073ddb84f04741451c0
Benjamin Kyhn <benkyhn@hotmail.com>
2019-12-20 08:21:11 +0100

    Opgave 2 tests bestået

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println(hello("András"));
-        System.out.println(hello(""));
 
     }
 
