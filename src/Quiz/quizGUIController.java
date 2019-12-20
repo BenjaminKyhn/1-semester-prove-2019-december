78ad2815be650644a24fbc25de5f05fec880bc39
Benjamin Kyhn <benj2605@edu.easj.dk>
2019-12-20 15:36:06 +0100

    Knapperne i GUI'en giver feedback, når du vælger et svar

--- src/Quiz/quizGUIController.java
+++ src/Quiz/quizGUIController.java
@@ -6,6 +6,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
 
@@ -28,6 +30,18 @@ public class quizGUIController extends Application implements Initializable {
     @FXML
     private Label svar3;
 
+    @FXML
+    private Label feedback;
+
+    @FXML
+    private Button button1;
+
+    @FXML
+    private Button button2;
+
+    @FXML
+    private Button button3;
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -49,5 +63,17 @@ public class quizGUIController extends Application implements Initializable {
         svar1.setText(svarMuligheder.get(0).toString());
         svar2.setText(svarMuligheder.get(1).toString());
         svar3.setText(svarMuligheder.get(2).toString());
+    }
+
+    public void button1(MouseEvent mouseEvent) {
+        feedback.setText("Du har svaret rigtigt András!");
+    }
+
+    public void button2(MouseEvent mouseEvent) {
+        feedback.setText("Du har desværre svaret forkert András :(");
+    }
+
+    public void button3(MouseEvent mouseEvent) {
+        feedback.setText("Du har desværre svaret forkert András :(");
     }
 }
