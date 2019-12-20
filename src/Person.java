cd87f39d8b4b477820cd81d9099dc9673343bf7a
Benjamin Kyhn <benkyhn@hotmail.com>
2019-12-20 09:23:33 +0100

    Opgave 5

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,7 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  * Developed by András Ács (acsandras@gmail.com)
  * Zealand / www.zealand.dk
@@ -9,24 +13,24 @@ public class Person {
 
     // TODO Ret nedenstående datatyper
     String navn; // Test Testesen
-    String alder; // 18
+    byte alder; // 18
     String by; // Næstved
-    String postNummer; // 4700
+    short postNummer; // 4700
     String vej; // Femøvej
     String husnummer; // 3B
     String nationalitet; // Dansk
-    String fritidsStringeresser; // Snitning, LOL, strikke
-    String smarthomeDevices; // Xbox, 3 Smartlamper, Køleskab m.m.
-    String hojde; // 1.56 m
-    String vaegt;  // 76 kg
-    String karaktergennemsnit; // 5.33
-    String eksamensKarakterer; // 7, 12, -3, 4, 2, 10
-    String opdateretDato; // Sun Jan 08 00:00:00 CET 1978
+    String[] fritidsStringeresser; // Snitning, LOL, strikke
+    ArrayList<String> smarthomeDevices; // Xbox, 3 Smartlamper, Køleskab m.m.
+    double hojde; // 1.56 m
+    double vaegt;  // 76 kg
+    double karaktergennemsnit; // 5.33
+    byte[] eksamensKarakterer; // 7, 12, -3, 4, 2, 10
+    Date opdateretDato; // Sun Jan 08 00:00:00 CET 1978
 
     // TODO Vis eksempler på typecasting
     @Override
     public String toString() {
-        return "Navn: " + navn + "By" + by;
+        return "Navn: " + navn + "\nBy: " + by + "\nAlder: " + alder + "\nHøjde: " + hojde + "\nVægt: " + (int)vaegt;
     }
 
     public static void main(String[] args) {
@@ -34,7 +38,34 @@ public class Person {
         Person person = new Person();
         person.navn = "Test Testesen";
         person.by = "Næstved";
+        person.postNummer = 4700;
+        person.vej = "Fenøvej";
+        person.husnummer = "3B";
+        person.nationalitet = "dansk";
+        person.fritidsStringeresser = new String[3];
+        person.fritidsStringeresser[0] = "snitning";
+        person.fritidsStringeresser[1] = "LoL";
+        person.fritidsStringeresser[2] = "strikke";
+        person.smarthomeDevices = new ArrayList<>();
+        person.smarthomeDevices.add("Xbox");
+        person.smarthomeDevices.add("3 Smartlamper");
+        person.smarthomeDevices.add("Køleskab");
+        person.hojde = 1.56;
+        person.vaegt = 76;
+        person.karaktergennemsnit = 5.33;
+        person.eksamensKarakterer = new byte[6];
+        person.eksamensKarakterer[0] = 7;
+        person.eksamensKarakterer[1] = 12;
+        person.eksamensKarakterer[2] = -3;
+        person.eksamensKarakterer[3] = 4;
+        person.eksamensKarakterer[4] = 2;
+        person.eksamensKarakterer[5] = 10;
+        person.opdateretDato = new Date(555555555);
         System.out.println();
+
+        System.out.println(person.opdateretDato);
+
+        System.out.println(person.toString());
     }
 
 }
